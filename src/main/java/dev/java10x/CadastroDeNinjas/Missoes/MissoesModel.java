c5474a322d949ea8f9bf5cb1a5f72168af1290a0
igor <idupalmieri@hotmail.com>
2025-04-23 13:53:13 -0300

    feat: Coloca o Lombok na classe para abstrucao

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,17 +2,25 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String Missao;
+
     private String dificuldade;
     //Uma missão varios ninjas
     @OneToMany(mappedBy = "missoes")
